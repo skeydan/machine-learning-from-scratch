6f443e7a6a4484807bde909493d4e96c62b76679
skeydan <keydana@gmx.de>
2016-07-25 21:58:08 +0200

    add LSH

---
+++ k_means.py
@@ -0,0 +1,20 @@
+from __future__ import division
+import numpy as np
+import sframe
+import matplotlib.pyplot as plt
+from scipy.sparse import csr_matrix
+from sklearn import metrics
+from copy import copy
+import time
+from itertools import combinations
+
+'''
+
+k means clustering
+
+'''
+
+
+# ------------------------------------------------------------------------------- 
+#                      Functions                                                #
+# -------------------------------------------------------------------------------
